be1d2a04706eb62aecfc57047a2ba4b913c4ee2c
jddavis23 <86057355+jddavis23@users.noreply.github.com>
2021-12-18 18:59:28 +0200

    Add files via upload
    Updated gnl with dynamically allocated memory for the static char array

---
+++ gnl/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdavis <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 12:03:17 by jdavis            #+#    #+#             */
+/*   Updated: 2021/12/18 16:50:34 by jdavis           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 9999
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
